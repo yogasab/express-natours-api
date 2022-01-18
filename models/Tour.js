31e14f70052bf9b09ee7b92eae8d5eee42a1269d
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-18 09:35:06 +0700

    Complete the Tour Model

--- models/Tour.js
+++ models/Tour.js
@@ -5,14 +5,55 @@ const TourSchema = mongoose.Schema({
 		type: String,
 		required: [true, "Name must be filled"],
 		unique: [true, "Name must be unique"],
+		trim: true,
 	},
-	price: {
+	duration: {
 		type: Number,
+		required: [true, "A tour must have durations"],
 	},
-	rating: {
+	maxGroupSize: {
+		type: Number,
+		required: [true, "A tour must have group size"],
+	},
+	difficulty: {
+		type: String,
+		required: [true, "A tour must have difficulty"],
+	},
+	ratingsAverage: {
 		type: Number,
 		default: 0,
 	},
+	ratingsQuantity: {
+		type: Number,
+		default: 0,
+	},
+	price: {
+		type: Number,
+		required: [true, "A tour must have price"],
+	},
+	priceDiscount: {
+		type: Number,
+	},
+	summary: {
+		type: String,
+		trim: true,
+		trim: [true, "A tour must have description"],
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	imageCover: {
+		type: String,
+		required: [true, "A tour must have cover image"],
+	},
+	// Images that contains array of image
+	images: [String],
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", TourSchema);
