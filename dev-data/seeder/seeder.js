bc82aa7e874c521696cd6be68e6fda0946c3e648
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-18 10:11:30 +0700

    Create Tour Seeder

---
+++ dev-data/seeder/seeder.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("../../models/Tour");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE_CLOUD_URI;
+
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: true,
+		useUnifiedTopology: true,
+	})
+	.then((con) => {
+		console.log(`Database connected successfully at ${con.connection.host}`);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+const tours = JSON.parse(fs.readFileSync("../data/tours-simple.json"));
+
+// Seed data
+const importData = async () => {
+	try {
+		await Tour.create(tours);
+		console.log("Tours seeded successfully");
+	} catch (error) {
+		console.log(error);
+	}
+	process.exit();
+};
+// Delete data
+const deleteData = async () => {
+	try {
+		await Tour.deleteMany();
+		console.log("Tours deleted successfully");
+	} catch (error) {
+		console.log(error);
+	}
+	process.exit();
+};
+
+if (process.argv[2] === "import" || process.argv[2] === "-i") {
+	importData();
+} else if (process.argv[2] === "delete" || process.argv[2] === "-d") {
+	deleteData();
+}
