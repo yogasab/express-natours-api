3e5e844f5167a7c2600bffe8903f3e9cd519cd8d
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-21 13:19:56 +0700

    Create middleware to handling unhandled routes

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-	console.log("Testing middleware");
-	next();
-});
+// app.use((req, res, next) => {
+// 	console.log("Testing middleware");
+// 	next();
+// });
 app.use((req, res, next) => {
 	// Set request time to header
 	req.requestTime = new Date().toISOString();
@@ -23,5 +23,13 @@ app.use((req, res, next) => {
 // Mounting Router
 app.use("/api/v1/tours", tourRouter);
 // app.use("/api/v1/users", userRouter);
+
+// Error handling for unavailable routes that defined in above
+app.all("*", (req, res, next) => {
+	res.status(404).json({
+		status: "Failed",
+		message: `The routes for ${req.originalUrl} is not found`,
+	});
+});
 
 module.exports = app;
