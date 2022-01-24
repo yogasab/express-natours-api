e25cc8c02aa88636eec5c58b45b9b99e70fbd552
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-24 14:46:11 +0700

    Create error handling for duplicate field mongo

--- middlewares/globalHandleError.js
+++ middlewares/globalHandleError.js
@@ -1,3 +1,5 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+
 const sendErrorDev = (res, err) => {
 	res.status(err.statusCode).json({
 		status: err.status,
@@ -19,6 +21,13 @@ const sendErrorProd = (res, err) => {
 	}
 };
 
+const handleDuplicateValueFieldDB = (error) => {
+	console.log(error.keyValue.name);
+	const value = error.keyValue.name;
+	const message = `Duplicate field value ${value}. Please use another value`;
+	return new ErrorResponse(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || "Error";
@@ -26,6 +35,8 @@ module.exports = (err, req, res, next) => {
 	if (process.env.NODE_ENV === "development") {
 		sendErrorDev(res, err);
 	} else if (process.env.NODE_ENV === "production") {
-		sendErrorProd(res, err);
+		let error = { ...err };
+		if (error.code === 11000) error = handleDuplicateValueFieldDB(error);
+		sendErrorProd(res, error);
 	}
 };
