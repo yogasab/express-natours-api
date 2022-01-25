e7bb6da7e114a3e90902f04ddb472f4377f7dc96
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-25 13:30:11 +0700

    Create password confirmation validation and hashing password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcryptjs = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
 
@@ -24,7 +25,25 @@ const UserSchema = new mongoose.Schema({
 	passwordConfirmation: {
 		type: String,
 		required: [true, "Please fill the password confirmation field"],
+		validate: {
+			validator: function (passwordConfirmation) {
+				return passwordConfirmation === this.password;
+			},
+			message: "Your password confirmation is not match. Please check again",
+		},
 	},
+});
+
+// Pre save middleware before save into User Model
+UserSchema.pre("save", async function (next) {
+	// Only run this function if password was actually modified
+	if (!this.isModified("password")) return next();
+	// Hash the password with salt of 12
+	this.password = await bcryptjs.hash(this.password, 12);
+	// Set passwordConfirmation to undefined, because its only need in field form to validate password
+	this.passwordConfirmation = undefined;
+
+	next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
