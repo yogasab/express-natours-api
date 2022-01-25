6a4c707b4b18f6708341d2e0319aba3b036aed38
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-25 09:04:10 +0700

    Create unhandledRejection for mongoose connection

--- server.js
+++ server.js
@@ -6,26 +6,28 @@ const mongoose = require("mongoose");
 const DB = process.env.DATABASE_CLOUD_URI;
 
 mongoose
-	.connect(
-		"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
-		{
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: true,
-			useUnifiedTopology: true,
-		}
-	)
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: true,
+		useUnifiedTopology: true,
+	})
 	.then((con) => {
 		console.log(
 			`Database connected successfully at ${con.connection.host} on ${process.env.NODE_ENV}`
 		);
-	})
-	.catch((err) => {
-		console.log(err);
 	});
 
 // Server
 const PORT = process.env.PORT | 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log(`Server running on localhost:${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+	console.log(err.name, err.message);
+	console.log(`UNHANDLED REJECTION! Shutting Down Server ...`);
+	server.close((err) => {
+		process.exit(1);
+	});
 });
