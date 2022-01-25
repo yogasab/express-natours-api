030d278df7811b0c16652056e970eb45afd5c955
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-25 08:58:09 +0700

    Create handle ValidationError mongoose

--- middlewares/globalHandleError.js
+++ middlewares/globalHandleError.js
@@ -16,15 +16,20 @@ const sendErrorProd = (res, err) => {
 			message: err.message,
 		});
 	} else {
-		console.error(`ERROR ${err}`);
 		res.status(500).json({ status: "Error", message: "Something went wrong" });
 	}
 };
 
 const handleDuplicateValueFieldDB = (error) => {
-	console.log(error.keyValue.name);
 	const value = error.keyValue.name;
 	const message = `Duplicate field value ${value}. Please use another value`;
+	return new ErrorResponse(message, 400);
+};
+
+const handleValidationErrorDB = (error) => {
+	const errors = Object.values(error.errors).map((el) => el.message);
+	const message = `Invalid input data. ${errors.join(`. `)}`;
+
 	return new ErrorResponse(message, 400);
 };
 
@@ -37,6 +42,9 @@ module.exports = (err, req, res, next) => {
 	} else if (process.env.NODE_ENV === "production") {
 		let error = { ...err };
 		if (error.code === 11000) error = handleDuplicateValueFieldDB(error);
+		console.log(error);
+		if (error._message === "Validation failed")
+			error = handleValidationErrorDB(error);
 		sendErrorProd(res, error);
 	}
 };
